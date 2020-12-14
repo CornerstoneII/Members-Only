47df3953184c5cd2c50844dbe15331754801083d
Igors78 <oleinikovsigors@gmail.com>
2020-12-14 11:58:29 +0200

    Format with rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    validates :user_id, presence: true
-    validates :title, :body, presence: true
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :title, :body, presence: true
 end
