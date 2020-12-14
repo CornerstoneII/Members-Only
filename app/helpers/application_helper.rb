bc375bb9b635cb2e29094629bd5e4e8a596f9148
Igors78 <oleinikovsigors@gmail.com>
2020-12-14 16:37:44 +0200

    Move logic for signin signup to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def check_user_for_post
     link_to 'New Post', new_post_path, class: 'nav-link' if user_signed_in?
   end
+
+  def authentic_user
+    if user_signed_in?
+      render inline: "Logged in as <strong><%= current_user.email %></strong>.
+
+        <%= link_to 'Logout', destroy_user_session_path, method: :delete, :class => 'navbar-link'  %>"
+
+    else
+      render inline: "<%= link_to 'Sign up', new_user_registration_path, :class => 'navbar-link'  %> |
+        <%= link_to 'Login', new_user_session_path, :class => 'navbar-link'  %>"
+    end
+  end
 end
